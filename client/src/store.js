bc494efa3cb7ebf4e60f99abb492616e9e0bcd46
Ayushman Lakshkar <ayushman.lakshkar7@gmail.com>
2024-01-21 06:11:25 +0530

    k

--- client/src/store.js
+++ client/src/store.js
@@ -1,7 +1,7 @@
 import {create} from 'zustand';
 
 export const useProbability = create((set) => ({
-    probability: 9,
+    probability: "",
     setProbability: (chance) => set({ probability: chance })
 }))
 
